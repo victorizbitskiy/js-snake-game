391db0919655f477f0283ec5b6e9010223698d32
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-19 20:54:33 +0300

    add snake drawing game.js

--- js/game.js
+++ js/game.js
@@ -1,19 +1,19 @@
-const canvas = document.getElementById("game")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById('game')
+const ctx = canvas.getContext('2d')
 
 const groundImg = new Image()
-groundImg.src = "img/ground.png"
+groundImg.src = 'img/ground.png'
 
 const foodImg = new Image()
-foodImg.src = "img/apple.png"
+foodImg.src = 'img/apple.png'
 
 let box = 32;
 let score = 0;
 
-// Задаем координаты еды
+// Set food coordinates
 let food = {
   x: Math.floor((Math.random() * 17 + 1)) * box,
-  y: Math.floor((Math.random() * 15 + 1)) * box
+  y: Math.floor((Math.random() * 15 + 3)) * box
 }
 
 let snake = []
@@ -23,11 +23,20 @@ snake[0] = {
 }
 
 function drawGame() {
-  // Рисуем фон
+  // Background drawing
   ctx.drawImage(groundImg, 0, 0)
 
-  // Рисуем еду
+  // Food drawing
   ctx.drawImage(foodImg, food.x , food.y)
+
+  // Snake drawing
+  for(let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = 'green'
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+  ctx.fillStyle = 'white'
+  ctx.font = '50px Arial'
+  ctx.fillText(score, box * 2.5, box * 1.7)
 }
 
 let game = setInterval(drawGame, 100)
