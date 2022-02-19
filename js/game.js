c14326ae4c75293febcbabba4f25d98cc0909890
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-19 20:41:26 +0300

    add food draw game.js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("game")
 const ctx = canvas.getContext("2d")
 
-const ground = new Image()
-ground.src = "img/ground.png"
+const groundImg = new Image()
+groundImg.src = "img/ground.png"
 
 const foodImg = new Image()
 foodImg.src = "img/apple.png"
@@ -10,8 +10,24 @@ foodImg.src = "img/apple.png"
 let box = 32;
 let score = 0;
 
+// Задаем координаты еды
+let food = {
+  x: Math.floor((Math.random() * 17 + 1)) * box,
+  y: Math.floor((Math.random() * 15 + 1)) * box
+}
+
+let snake = []
+snake[0] = {
+  x: 9 * box,
+  y: 10 * box
+}
+
 function drawGame() {
-  ctx.drawImage(ground, 0, 0)
+  // Рисуем фон
+  ctx.drawImage(groundImg, 0, 0)
+
+  // Рисуем еду
+  ctx.drawImage(foodImg, food.x , food.y)
 }
 
 let game = setInterval(drawGame, 100)
