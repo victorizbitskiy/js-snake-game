637b89215c9c2d43df26390db9644ce351441952
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-20 20:02:06 +0300

    refactoring game.js

--- js/game.js
+++ js/game.js
@@ -1,106 +1,134 @@
 const canvas = document.getElementById('game')
-const ctx = canvas.getContext('2d')
+const context = canvas.getContext('2d')
 
-const groundImg = new Image()
-groundImg.src = 'img/ground.png'
+const backgroundImg = new Image()
+backgroundImg.src = 'img/background.png'
 
 const foodImg = new Image()
 foodImg.src = 'img/apple.png'
 
-let box = 32;
+let boxScale = 32;
 let score = 0;
 
-// Set food coordinates
-let food = {
-  x: Math.floor((Math.random() * 17 + 1)) * box,
-  y: Math.floor((Math.random() * 15 + 3)) * box
+let food = createNewFood()
+let snake = new Array(getStartPosition())
+let direction
+
+function createNewFood() {
+  return {
+    x: Math.floor((Math.random() * 17 + 1)) * boxScale,
+    y: Math.floor((Math.random() * 15 + 3)) * boxScale
+  }
 }
 
-let snake = []
-snake[0] = {
-  x: 9 * box,
-  y: 10 * box
+function getStartPosition() {
+  return {
+    x: 9 * boxScale,
+    y: 10 * boxScale
+  }
 }
 
-document.addEventListener('keydown', direction)
+function isEatTail(snakeArray) {
+  if (snakeArray.length === 1)
+    return false
 
-let dir
-
-function direction(event) {
-  console.log(event)
-  if (event.key === 'ArrowUp' && dir !== 'down')
-    dir = 'up'
-  else if (event.key === 'ArrowRight' && dir !== 'left')
-    dir = 'right'
-  else if (event.key === 'ArrowDown' && dir !== 'up')
-    dir = 'down'
-  else if (event.key === 'ArrowLeft' && dir !== 'right')
-    dir = 'left'
-}
-
-function eatTail(head, SnakeArray) {
-  for (let i = 0; i < SnakeArray.length; i++) {
-    if (head.x === SnakeArray[i].x && head.y === SnakeArray[i].y) {
-      ctx.fillStyle = 'red'
-      ctx.fillRect(SnakeArray[i].x, SnakeArray[i].y, box, box)
-      clearInterval(game)
+  for (let i = 1; i < snakeArray.length; i++) {
+    if (snakeArray[0].x === snakeArray[i].x && snakeArray[0].y === snakeArray[i].y) {
+      return true
     }
   }
 }
+
+function isCollision(head) {
+  if (head.x < boxScale || head.x > boxScale * 17
+    || head.y < 3 * boxScale || head.y > boxScale * 17) {
+    return true
+  }
+}
+
+function getNextHead(snakeHead) {
+  // Save current snake head coordinates 
+  let snakeHeadX = snakeHead.x
+  let snakeHeadY = snakeHead.y
+
+  if (direction === 'left') snakeHeadX -= boxScale
+  if (direction === 'right') snakeHeadX += boxScale
+  if (direction === 'up') snakeHeadY -= boxScale
+  if (direction === 'down') snakeHeadY += boxScale
+
+  // Return new snake head coordinates
+  return {
+    x: snakeHeadX,
+    y: snakeHeadY
+  }
+}
+
+function drawScore() {
+  context.fillStyle = 'white'
+  context.font = '50px Arial'
+  context.fillText(score, boxScale * 2.5, boxScale * 1.7)
+}
+
+function drawSnake() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = i === 0 ? 'green ' : 'forestgreen '
+    context.fillRect(snake[i].x, snake[i].y, boxScale, boxScale)
+  }
+}
+
+function drawFood() {
+  context.drawImage(foodImg, food.x, food.y)
+}
+
+function isFoodEaten() {
+  if (snake[0].x === food.x && snake[0].y === food.y) {
+    return true
+  }
+}
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowUp' && direction !== 'down')
+    direction = 'up'
+  else if (event.key === 'ArrowRight' && direction !== 'left')
+    direction = 'right'
+  else if (event.key === 'ArrowDown' && direction !== 'up')
+    direction = 'down'
+  else if (event.key === 'ArrowLeft' && direction !== 'right')
+    direction = 'left'
+})
 
 function drawGame() {
-  // Background drawing
-  ctx.drawImage(groundImg, 0, 0)
+  // Add background to context
+  context.drawImage(backgroundImg, 0, 0)
 
-  // Food drawing
-  ctx.drawImage(foodImg, food.x, food.y)
+  drawScore()
+  drawFood()
+  drawSnake()
 
-  // Snake drawing
-  for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = i === 0 ? 'green ' : 'forestgreen '
-    ctx.fillRect(snake[i].x, snake[i].y, box, box)
-  }
+  let newHead = getNextHead(snake[0])
 
-  ctx.fillStyle = 'white'
-  ctx.font = '50px Arial'
-  ctx.fillText(score, box * 2.5, box * 1.7)
+  if (!isCollision(newHead)) {
 
-  // Save snake coordinates 
-  let snakeX = snake[0].x
-  let snakeY = snake[0].y
-
-  // Eating food process
-  if (snakeX === food.x && snakeY === food.y) {
-    score++
-    food = {
-      x: Math.floor((Math.random() * 17 + 1)) * box,
-      y: Math.floor((Math.random() * 15 + 3)) * box
+    if (isEatTail(snake)) {
+      context.fillStyle = 'red'
+      context.fillRect(snake[0].x, snake[0].y, boxScale, boxScale)
+      clearInterval(game)
     }
+
+    snake.unshift(newHead)
+
+    if (isFoodEaten()) {
+      score++
+      food = createNewFood()
+    } else {
+      snake.pop()
+    }
+
   } else {
-    snake.pop()
-  }
-
-  if (snakeX < box || snakeX > box * 17
-    || snakeY < 3 * box || snakeY > box * 17) {
-
-    ctx.fillStyle = 'red'
-    ctx.fillRect(snakeX, snakeY, box, box)
+    context.fillStyle = 'red'
+    context.fillRect(snake[0].x, snake[0].y, boxScale, boxScale)
     clearInterval(game)
   }
-
-  if (dir == 'left') snakeX -= box
-  if (dir == 'right') snakeX += box
-  if (dir == 'up') snakeY -= box
-  if (dir == 'down') snakeY += box
-
-  let newHead = {
-    x: snakeX,
-    y: snakeY
-  }
-
-  eatTail(newHead, snake)
-  snake.unshift(newHead)
-
 }
 
 let game = setInterval(drawGame, 200)
